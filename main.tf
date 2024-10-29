c09a13d0bfb53e8f9e48598a8e3ed979bd17a5f2
Muhammad Atta Ur Rehman <73884338+themuhammadattaurrehman@users.noreply.github.com>
2024-10-29 19:49:40 +0500

    changes in main file

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Update this with a valid AMI ID
+  ami           = "ami-06b21ccaeff8cd686"  # Update this with a valid AMI ID
   instance_type = "t2.micro"               # Change instance type as needed
 
   tags = {
